d15de89c942cf48937529f05dc7ac4cd4fc174bc
elfinamk24 <karima.elfina@gmail.com>
2022-04-08 20:42:09 +0700

    Initial Commit

---
+++ MainCode.py
@@ -0,0 +1,12 @@
+##################
+##    IMPORT    ##
+##################
+import pandas as pd
+import streamlit as st
+import altair as alt
+from PIL import Image
+
+##################
+## PAGE TITLE   ##
+##################
+image = Ima
